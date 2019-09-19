6d5bc029ce3dd3912351e39bfc284424f02f19ea
Admin <naveenkaushal441@gmail.com>
2019-09-19 11:08:55 +0530

    DashBoard Added

---
+++ NewDashBoard/Main.java
@@ -0,0 +1,31 @@
+
+package NewDashBoard;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root=FXMLLoader.load(getClass().getResource("ViewDash.fxml")); 
+			Scene scene = new Scene(root,536,540);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			                                                                                                             
+		} catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
