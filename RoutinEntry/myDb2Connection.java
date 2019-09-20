107ceec34ff0329af48cd927016f3c82c1258c8a
Admin <naveenkaushal441@gmail.com>
2019-09-20 17:17:00 +0530

    Connection added

---
+++ RoutinEntry/myDb2Connection.java
@@ -0,0 +1,32 @@
+package RoutinEntry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class myDb2Connection
+{
+	static Connection doConnect()
+	{
+		Connection con=null; //LRV
+		try {
+			
+			 Class.forName("com.mysql.jdbc.Driver");
+			 //														    databaseName      password
+			 con=	DriverManager.getConnection("jdbc:mysql://localhost/info","root","Nav1een.p");
+			 System.out.println("Connected"); 
+		 
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public static void main(String[] args) 
+	{
+		doConnect();
+
+	}
+
+}
